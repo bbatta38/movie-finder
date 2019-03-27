e702647f70cc06d4fd3336f4ba341318049f3678
ByungSoo Kim <bbatta38@gmail.com>
2019-03-27 23:54:36 +1300

    styling navigation

--- src/components/Navigation/presenter.js
+++ src/components/Navigation/presenter.js
@@ -1,16 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTicketAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = props => (
-  <div>
-    <img src="" alt="Movie Finder" />
-    <div>
+  <div className={styles.container}>
+    <span className={styles.logo}>
+      <FontAwesomeIcon icon={faTicketAlt} color="white" size="2x" />
+      <span className={styles.logoText}>Movie Finder</span>
+    </span>
+    <div className={styles.searchContainer}>
       <form onSubmit={props.onSubmit}>
         <input
+          className={styles.searchInput}
           type="search"
           value={props.searchText}
           onChange={props.onChange}
+          placeholder="movie name"
         />
+        <button
+          type="button"
+          onClick={props.onSubmit}
+          className={styles.button}
+        >
+          <FontAwesomeIcon
+            icon={faSearch}
+            color="white"
+            className={styles.searchIcon}
+          />
+        </button>
       </form>
     </div>
   </div>
